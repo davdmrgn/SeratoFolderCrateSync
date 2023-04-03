d0c188da6e6ad3d4cb811fbe4a1013a5c1a72076
davdmrgn <davdmrgn@gmail.com>
2023-04-03 00:14:27 -0400

    Make it a proper function

--- encode.py
+++ encode.py
@@ -10,30 +10,29 @@ config.read('config.txt')
 library = homedir + config['paths']['library']
 music = homedir + config['paths']['music']
 
-def encode(data):
-  key = data[0].encode('utf-8')
-  value = data[1].encode('utf-16-be')
-  length = struct.pack('>I', len(value))
-  return(key + length + value)
-
 # data is an existing decoded crate
+def encode(data):
+  result = b''
+  i = 0
+  for line in data:
+    try:
+      key = line[0].encode('utf-8')
+      if isinstance(line[1], list):
+        sub_list = line[1][0]
+        sub_key = sub_list[0].encode('utf-8')
+        sub_value = sub_list[1].encode('utf-16-be')
+        sub_length = struct.pack('>I', len(sub_value))
+        value = (sub_key + sub_length + sub_value)
+      else:
+        value = line[1].encode('utf-16-be')
+      length = struct.pack('>I', len(value))
+      result += (key + length + value)
+      i += 1
+    except:
+      print('ERROR: i: {}, key: {}, length: {}, value: {}'.format(i, key, length, value))
+      break
+  return(result)
 
-i = 0
-x = b''
-for line in data:
-  try:
-    key = line[0].encode('utf-8')
-    if isinstance(line[1], list):
-      value = encode(line[1][0])
-    else:
-      value = line[1].encode('utf-16-be')
-    length = struct.pack('>I', len(value))
-    x += (key + length + value)
-    i += 1
-  except:
-    print('ERROR: i: {}, key: {}, length: {}, value: {}'.format(i, key, length, value))
-    break
-
-file = library + '/Subcrates/This better work2.crate'
+file = library + '/Subcrates/This better work4.crate'
 with open(file, 'wb') as crate:
   crate.write(x)
