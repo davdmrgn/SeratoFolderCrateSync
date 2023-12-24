37c06f210b1e44f3f47a91d79cb98ddf5b1e4dc9
davdmrgn <davd.mrgn@gmail.com>
2023-12-24 09:43:40 -0500

    Decoded 2 files

---
+++ decode_db.py
@@ -0,0 +1,42 @@
+import configparser
+import os
+import re
+import struct
+import time
+from datetime import datetime
+import logging
+import shutil
+
+serato_database = '/Users/dave/Documents/database V2'
+
+with open(serato_database, 'rb') as db:
+  db_binary = db.read()
+
+data = db_binary
+
+def decode(input):
+  i = 0
+  while i < 1000:# len(input):
+    key = input[i:i+4].decode('utf-8')
+    if re.match('vrsn|ttyp|pfil|tsng|tart|talb|tgen|tlen|tsiz|tbit|tbpm|tcom|ttyr|tad|tkey', key):
+      length = struct.unpack('>I', input[i+4:i+8])[0]
+      binary = input[i+8:i+8 + length]
+      binary_data = binary.decode('utf-16-be')
+      print(key, binary_data)
+    elif re.match('otrk', key):
+      length = struct.unpack('>I', input[i+4:i+8])[0]
+      binary = input[i+8:i+8 + length]
+      binary_data = decode(binary)
+    # elif key == '':
+    #   binary_data = binary
+    # else:
+    #   length = struct.unpack('>I', input[i+4:i+8])[0]
+    #   binary = input[i+8:i+8 + length]
+    #   if re.match('bhrt|uadd|ulbl|utme\sbav|bmis|bply|blop|bitu|bovc|bcrt|biro|bwlb|bwll|buns|bbgl|bkrk|bstm', key):
+    #     binary_data = binary
+    #   # else:
+    #   #   binary_data = binary.decode('utf-16-be')
+    i += 8 + length
+  return(key, binary_data)
+
+decode(data)
