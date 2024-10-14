320d0e839232e483cc89c2b34c9a0435f4ffae83
davdmrgn <davdmrgn@gmail.com>
2024-10-14 19:00:32 -0400

    Fix log filename

--- modules/Logger.py
+++ modules/Logger.py
@@ -1,11 +1,11 @@
-import os, logging
+import os, sys, logging
 from datetime import datetime
 
 
 def Logger(data):
   """Save log file to Serato database"""
   db_path = data['db_path']
-  log_filename = f'{os.path.splitext(os.path.basename(__file__))[0]}-{datetime.now().strftime("%Y-%m-%d")}.log'
+  log_filename = f'{os.path.splitext(os.path.basename(sys.argv[0]))[0]}-{datetime.now().strftime("%Y%m%d")}.log'
   log = f'{db_path}/Logs/{log_filename}'
   logging.basicConfig(filename=log, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', force=True)
   console = logging.StreamHandler()
